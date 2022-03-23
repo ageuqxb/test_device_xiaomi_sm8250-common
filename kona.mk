583bd9e106517167088a5867f1ea11eed2b9fd36
ahmedtohamy1 <1ahmed.tohamy@gmail.com>
2022-03-23 12:23:34 +0000

    Sm8250-common: Remove packages at build time

--- kona.mk
+++ kona.mk
@@ -7,6 +7,10 @@
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
